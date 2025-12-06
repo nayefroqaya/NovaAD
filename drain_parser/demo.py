f9bc4d985d3c5fad11de97e996a97c52188c2df6
Nayef Roqaya <n.roqaya@gmail.com>
2025-12-06 15:57:35 +0100

    Save changes to demo.py

--- drain_parser/demo.py
+++ drain_parser/demo.py
@@ -4,16 +4,37 @@ import sys
 sys.path.append('../../')
 from Drain import LogParser
 
+
+#==========================================OS
+# This part is for HDFS dataset includes : Input dir, output dir, name of log file, log format(all formats in Benchmark file).
+input_dir  = '../datasets/OpenStack/' # The input directory of log file
+output_dir = '../datasets/OpenStack/'  # The output directory of parsing results
+log_file   = 'openstack_normal2.log'  # The input log file name
+log_format = '<Logrecord> <Date> <Time> <Pid> <Level> <Component> \[<ADDR>\] <Content>'  # OS  log format
+# Regular expression list for optional preprocessing (default: [])
+regex      =[r"((\d+\.){3}\d+,?)+", r"/.+?\s", r"\d+"]
+st         = 0.5  # Similarity threshold
+depth      = 5  # Depth of all leaf nodes
+#==========================================TH
+# This part is for HDFS dataset includes : Input dir, output dir, name of log file, log format(all formats in Benchmark file).
+#input_dir  = '../datasets/TH/' # The input directory of log file
+#output_dir = '../datasets/TH/'  # The output directory of parsing results
+#log_file   = 'Thunderbird_10MB_subset1.log'  # The input log file name
+#log_format = '<Label> <Timestamp> <Date> <User> <Month> <Day> <Time> <Location> <Component>(\[<PID>\])?: <Content>'  # TH  log format
+# Regular expression list for optional preprocessing (default: [])
+#regex      = [r"(\d+\.){3}\d+"]
+#st         = 0.5  # Similarity threshold
+#depth      = 4  # Depth of all leaf nodes
 #==========================================HDFS
 # This part is for HDFS dataset includes : Input dir, output dir, name of log file, log format(all formats in Benchmark file).
-input_dir  = '../datasets/HDFS/' # The input directory of log file
-output_dir = '../datasets/HDFS/'  # The output directory of parsing results
-log_file   = 'HDFS.log'  # The input log file name
-log_format = '<Date> <Time> <Pid> <Level> <Component>: <Content>'  # HDFS log format
-# Regular expression list for optional preprocessing (default: [])
-regex      = [r"blk_-?\d+", r"(\d+\.){3}\d+(:\d+)?"]
-st         = 0.5  # Similarity threshold
-depth      = 4  # Depth of all leaf nodes
+#input_dir  = '../datasets/HDFS/' # The input directory of log file
+#output_dir = '../datasets/HDFS/'  # The output directory of parsing results
+#log_file   = 'HDFS.log'  # The input log file name
+#log_format = '<Date> <Time> <Pid> <Level> <Component>: <Content>'  # HDFS log format
+## Regular expression list for optional preprocessing (default: [])
+#regex      = [r"blk_-?\d+", r"(\d+\.){3}\d+(:\d+)?"]
+#st         = 0.5  # Similarity threshold
+#depth      = 4  # Depth of all leaf nodes
  
 #==========================================BGL
 # This part is for BGL dataset includes : Input dir, output dir, name of log file, log format(all formats in Benchmark file).
