95020a67712a2b38475c5256ca58b2b42ab01889
nayefroqaya <n.roqaya@gmail.com>
2026-01-09 20:12:56 +0100

    check label

--- src/logdata_read.py
+++ src/logdata_read.py
@@ -279,7 +279,7 @@ class LogdataRead:
             coverage = vc[vc >= 10].sum() / vc.sum()
             print(f"Log coverage by templates with ≥10 occurrences: {coverage:.2%}")
 
-            df['LogType'] = df['Label'].apply(lambda x: 'Normal' if x == '-' else 'Anomaly')
+            df['LogType'] = df['Original_Label'].apply(lambda x: 'Normal' if x == '-' else 'Anomaly')
             df['LogType'].value_counts(normalize=True) * 100
             print(df['LogType'].value_counts())
 
