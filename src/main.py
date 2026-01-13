534929b2f80bae3b18d43f7961fda6718bd29ebf
nayefroqaya <n.roqaya@gmail.com>
2026-01-13 08:40:20 +0100

    stable and Mix dataset

--- src/main.py
+++ src/main.py
@@ -54,14 +54,14 @@ def main():
 #    DATASET = 'BGL'
 #=======
 #<<<<<<< HEAD
-    DATASET = 'SP_100MB'
+    DATASET = 'BGL'
 #=======
 #    DATASET = 'HDFS'
 #>>>>>>> 66197ed (check label)
 #>>>>>>> 42c37e9 (update input name)
     DATASETS_FOLDER = 'datasets'
     Round= '1'
-    mode='S'  # M multi classifier - S single classifier
+    mode='M'  # M multi classifier - S single classifier
     Mix_or_stable='0'  # 0 Full stable subset  / 1 mix subset
 
 
