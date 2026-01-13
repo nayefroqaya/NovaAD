8e4f4da7139b07c0b2fd6bf1aea46069cbd882a1
nayefroqaya <n.roqaya@gmail.com>
2026-01-13 08:26:21 +0100

    stable and Mix dataset

--- src/features_engineering.py
+++ src/features_engineering.py
@@ -36,7 +36,8 @@ class FeaturesEngineering:
 
               # Return zeros with the same length as any feature vector
 #              return np.zeros(len(feature_list[0]), dtype=np.float32)
-          return np.mean(np.stack(arrays), axis=0)
+          #return np.mean(np.stack(arrays), axis=0)
+          return np.max(np.stack(arrays), axis=0)
 
         log_normal_labelled = df_train_with_test_with_val[df_train_with_test_with_val['Temp_label'] == 0].copy()
         # 2️⃣ Remove UNKNOWN blocks (case-insensitive)
