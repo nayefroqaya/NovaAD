e4f9429865a45eba3afb80028564b94ba8d89fda
nayefroqaya <n.roqaya@gmail.com>
2026-02-25 14:18:35 +0100

    update Novelty

--- src/main.py
+++ src/main.py
@@ -51,7 +51,7 @@ def main():
 
     # ---------------- Project configuration ----------------
 
-    DATASET = 'HDFS'
+    DATASET = 'BGL'
     DATASETS_FOLDER = 'datasets'
     Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
