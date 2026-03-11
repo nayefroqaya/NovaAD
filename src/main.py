891d2c800e0c55188c13bfefc90e92749f8fc06b
nayefroqaya <n.roqaya@gmail.com>
2026-03-11 13:20:40 +0100

    update Novelty

--- src/main.py
+++ src/main.py
@@ -52,7 +52,7 @@ def main():
 
     DATASET = 'BGL'
     DATASETS_FOLDER = 'datasets'
-    Round = '1'
+    Round = '2'
     mode = 'M'  # M multi classifier - S single classifier
     Mix_or_stable = '0'  # 0 Full stable subset  / 1 mix subset
 
