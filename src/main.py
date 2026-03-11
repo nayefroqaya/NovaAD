3311546d2216941e41280fd210fb4fb21928d8e2
nayefroqaya <n.roqaya@gmail.com>
2026-03-11 14:59:16 +0100

    update Novelty

--- src/main.py
+++ src/main.py
@@ -52,7 +52,7 @@ def main():
 
     DATASET = 'SP_150MB'
     DATASETS_FOLDER = 'datasets'
-    Round = '2'
+    Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
     Mix_or_stable = '0'  # 0 Full stable subset  / 1 mix subset
 
