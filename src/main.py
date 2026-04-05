3840ac419d3064b7f464d996c4855939129be433
nayefroqaya <n.roqaya@gmail.com>
2026-04-05 19:01:03 +0200

    update info

--- src/main.py
+++ src/main.py
@@ -97,9 +97,9 @@ def main():
 
     # ---------------- Project configuration ----------------
 
-    DATASET = 'TH_1G'
+    DATASET = 'TH_2G'
     DATASETS_FOLDER = 'datasets'
-    Round = '2'
+    Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
     Mix_or_stable = '0'  # 0 Full stable subset  / 1 mix subset
 
@@ -122,7 +122,7 @@ def main():
     model_evaluation_obj = ModelEvaluation()
     utilities_obj = Utilities()
 
-    '''
+
     # ---------------- Data as CSV ----------------
     #logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
     #print(' Reading the file was done successfully ')
@@ -171,7 +171,7 @@ def main():
         final_train_with_test_with_val)
 
     exit()
-    '''
+
 
 
     final_train_with_test_with_val = pd.read_pickle(PRE_FINAL_GLOBAL_FEATURES_PKL_PATH)
