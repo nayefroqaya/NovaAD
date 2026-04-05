0a34d05cb29b43eff7de2e8523844726d162d945
nayefroqaya <n.roqaya@gmail.com>
2026-04-05 22:21:29 +0200

    update info

--- src/main.py
+++ src/main.py
@@ -97,9 +97,9 @@ def main():
 
     # ---------------- Project configuration ----------------
 
-    DATASET = 'SP_150MB'
+    DATASET = 'HDFS'
     DATASETS_FOLDER = 'datasets'
-    Round = '3'
+    Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
     Mix_or_stable = '0'  # 0 Full stable subset  / 1 mix subset
 
@@ -121,7 +121,7 @@ def main():
     anomaly_detection_obj = AnomalyDetector()
     model_evaluation_obj = ModelEvaluation()
     utilities_obj = Utilities()
-    '''
+
 
     # ---------------- Data as CSV ----------------
     #logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
@@ -173,7 +173,7 @@ def main():
         final_train_with_test_with_val)
 
     exit()
-    '''
+
 
 
 
