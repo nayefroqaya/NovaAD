e3dc6cd5b9a5da674d1c5dadd88472c81bf0d555
nayefroqaya <n.roqaya@gmail.com>
2026-04-05 16:13:48 +0200

    update info

--- src/main.py
+++ src/main.py
@@ -97,9 +97,9 @@ def main():
 
     # ---------------- Project configuration ----------------
 
-    DATASET = 'BGL'
+    DATASET = 'TH_1G'
     DATASETS_FOLDER = 'datasets'
-    Round = '3'
+    Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
     Mix_or_stable = '0'  # 0 Full stable subset  / 1 mix subset
 
@@ -121,7 +121,7 @@ def main():
     anomaly_detection_obj = AnomalyDetector()
     model_evaluation_obj = ModelEvaluation()
     utilities_obj = Utilities()
-    '''
+
 
     # ---------------- Data as CSV ----------------
     #logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
@@ -171,7 +171,7 @@ def main():
         final_train_with_test_with_val)
 
     exit()
-    '''
+
 
     final_train_with_test_with_val = pd.read_pickle(PRE_FINAL_GLOBAL_FEATURES_PKL_PATH)
     final_train_with_test_with_val.info()
