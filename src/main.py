7ba0e546755dc9676a06e88034e2749501f7c182
nayefroqaya <n.roqaya@gmail.com>
2026-04-06 18:21:15 +0200

    update info

--- src/main.py
+++ src/main.py
@@ -99,7 +99,7 @@ def main():
 
     DATASET = 'HDFS'
     DATASETS_FOLDER = 'datasets'
-    Round = '3'
+    Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
     Mix_or_stable = '0'  # 0 Full stable subset  / 1 mix subset
 
@@ -109,10 +109,16 @@ def main():
     # path for full data CSV file
     ALL_DATASET_CSV_PATH = f'../{DATASETS_FOLDER}/{DATASET}/{DATASET}.csv'
 
-    # Path features results
-    DOC_TOPIC_DF_PATH = f'../{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_All_doc_topic_df.pkl'
-    SENTIMENT_DF_PATH = f'../{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_All_sentiment_df.pkl'
-    PRE_FINAL_GLOBAL_FEATURES_PKL_PATH = f'../{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_All_pre_final_global_features.pkl'
+    # Path features results- paper1
+    #DOC_TOPIC_DF_PATH = f'../{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_All_doc_topic_df.pkl'
+    #SENTIMENT_DF_PATH = f'../{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_All_sentiment_df.pkl'
+    #PRE_FINAL_GLOBAL_FEATURES_PKL_PATH = f'../{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_All_pre_final_global_features.pkl'
+
+    # Path features results- paper2:
+    DOC_TOPIC_DF_PATH = f'../../NovaAD_Plus/{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_All_doc_topic_df.pkl'
+    SENTIMENT_DF_PATH = f'../../NovaAD_Plus/{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_All_sentiment_df.pkl'
+    PRE_FINAL_GLOBAL_FEATURES_PKL_PATH = f'../../NovaAD_Plus/{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_All_pre_final_global_features.pkl'
+
 
     # ---------------- Initialize classes ----------------
     logdata_read_obj = LogdataRead()
@@ -122,7 +128,7 @@ def main():
     model_evaluation_obj = ModelEvaluation()
     utilities_obj = Utilities()
 
-    '''
+
     # ---------------- Data as CSV ----------------
     #logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
     #print(' Reading the file was done successfully ')
@@ -151,15 +157,15 @@ def main():
         print(f"{GRAY}Processing normal data portion in the dataset...{RESET}")
         save_path = os.path.join(f"../datasets/{DATASET}", f"{Round}_{DATASET}_Splitted_Datasets")
 
-    # first paper :
-    train_df = pd.read_pickle(os.path.join(save_path, "train_df.pkl"))
-    val_df = pd.read_pickle(os.path.join(save_path, "val_df.pkl"))
-    test_df = pd.read_pickle(os.path.join(save_path, "test_df.pkl"))
+    # first paper :*****************
+    #train_df = pd.read_pickle(os.path.join(save_path, "train_df.pkl"))
+    #val_df = pd.read_pickle(os.path.join(save_path, "val_df.pkl"))
+    #test_df = pd.read_pickle(os.path.join(save_path, "test_df.pkl"))
 
-    # Second paper :
-    #test_df = '../NovaAD_Plus/datasets/HDFS/1_HDFS_Splitted_Datasets/train_df.pkl'
-    #train_df = '../NovaAD_Plus/datasets/HDFS/1_HDFS_Splitted_Datasets/test_df.pkl'
-    #val_df = '../NovaAD_Plus/datasets/HDFS/1_HDFS_Splitted_Datasets/val_df.pkl'
+    # Second paper :************
+    train_df = f"../../NovaAD_Plus/{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_Splitted_Datasets/train_df.pkl"
+    test_df = f"../../NovaAD_Plus/{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_Splitted_Datasets/test_df.pkl"
+    val_df = f"../../NovaAD_Plus/{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_Splitted_Datasets/val_df.pkl"
 
     final_train_with_test_with_val = utilities_obj.processing_data_portion(train_df, val_df, test_df)
     # exit()
@@ -173,7 +179,7 @@ def main():
         final_train_with_test_with_val)
 
     exit()
-    '''
+
 
 
 
