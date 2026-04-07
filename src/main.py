3e65320c1e58e369850f69d3f24d8e060f66890d
nayefroqaya <n.roqaya@gmail.com>
2026-04-07 19:02:36 +0200

    update info

--- src/main.py
+++ src/main.py
@@ -99,7 +99,7 @@ def main():
 
     DATASET = 'SP_100MB'
     DATASETS_FOLDER = 'datasets'
-    Round = '3'
+    Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
     Mix_or_stable = '0'  # 0 Full stable subset  / 1 mix subset
 
@@ -128,7 +128,7 @@ def main():
     model_evaluation_obj = ModelEvaluation()
     utilities_obj = Utilities()
 
-
+    '''
     # ---------------- Data as CSV ----------------
     #logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
     #print(' Reading the file was done successfully ')
@@ -174,6 +174,7 @@ def main():
     final_train_with_test_with_val = utilities_obj.processing_data_portion(train_df, val_df, test_df)
     # exit()
     
+    
    
 
     # ---------------- Features Extracting ----------------
@@ -183,12 +184,7 @@ def main():
         final_train_with_test_with_val)
 
     exit()
-
-
-
-
-
-
+    '''
 
 
 
