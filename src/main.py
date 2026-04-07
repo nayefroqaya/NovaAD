8f1732b7f87df0a9e55a9fcaad3f7d652bcd3bb0
nayefroqaya <n.roqaya@gmail.com>
2026-04-07 17:17:20 +0200

    update info

--- src/main.py
+++ src/main.py
@@ -99,7 +99,7 @@ def main():
 
     DATASET = 'TH_1G'
     DATASETS_FOLDER = 'datasets'
-    Round = '3'
+    Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
     Mix_or_stable = '0'  # 0 Full stable subset  / 1 mix subset
 
@@ -128,7 +128,7 @@ def main():
     model_evaluation_obj = ModelEvaluation()
     utilities_obj = Utilities()
 
-
+    '''
     # ---------------- Data as CSV ----------------
     #logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
     #print(' Reading the file was done successfully ')
@@ -183,6 +183,7 @@ def main():
         final_train_with_test_with_val)
 
     exit()
+    '''
 
 
 
