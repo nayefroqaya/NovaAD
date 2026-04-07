01f23e416224a00ce97f8575eee0a65944e56290
nayefroqaya <n.roqaya@gmail.com>
2026-04-07 20:33:25 +0200

    update info

--- src/main.py
+++ src/main.py
@@ -97,9 +97,9 @@ def main():
 
     # ---------------- Project configuration ----------------
 
-    DATASET = 'SP_150MB'
+    DATASET = 'SP_100MB'
     DATASETS_FOLDER = 'datasets'
-    Round = '3'
+    Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
     Mix_or_stable = '0'  # 0 Full stable subset  / 1 mix subset
 
