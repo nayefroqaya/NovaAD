e60a1315270b45b2fe3bf543f0fa65ccd660760a
nayefroqaya <n.roqaya@gmail.com>
2026-04-10 11:27:17 +0200

    reformat the code

--- src/main.py
+++ src/main.py
@@ -97,7 +97,7 @@ def main():
 
     # ---------------- Project configuration ----------------
 
-    DATASET = 'TH_1G'
+    DATASET = 'SP_100MB'
     DATASETS_FOLDER = 'datasets'
     Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
