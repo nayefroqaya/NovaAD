af6063ae2b8b371a9518a7f5479f1e1e50e5e24b
nayefroqaya <n.roqaya@gmail.com>
2026-04-13 22:08:36 +0200

    update in the  dataset code

--- src/main.py
+++ src/main.py
@@ -107,7 +107,8 @@ def main():
     ALL_DATASET_LOG_PATH = f'../{DATASETS_FOLDER}/{DATASET}/{DATASET}.LOG'
 
     # path for full data CSV file
-    ALL_DATASET_CSV_PATH = f'../{DATASETS_FOLDER}/{DATASET}/{DATASET}.csv'
+    #ALL_DATASET_CSV_PATH = f'../{DATASETS_FOLDER}/{DATASET}/{DATASET}.csv' # data first paper
+    ALL_DATASET_CSV_PATH = f'../../NovaAD_Plus/{DATASETS_FOLDER}/{DATASET}/{DATASET}.csv' # data second paper
 
     # Path features results- paper1
     #DOC_TOPIC_DF_PATH = f'../{DATASETS_FOLDER}/{DATASET}/{Round}_{DATASET}_All_doc_topic_df.pkl'
@@ -130,9 +131,9 @@ def main():
 
 
     # ---------------- Data as CSV ----------------
-    #logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
-    #print(' Reading the file was done successfully ')
-    #exit()
+    logdata_read_obj.read_original_data_log_from_log_to_csv(DATASET, ALL_DATASET_CSV_PATH)
+    print(' Reading the file was done successfully ')
+    exit()
     
 
     # ---------------- Dataset Splitting ----------------
