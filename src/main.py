162f5fd67f66f5f24b675cacf6cd0c336f0a719c
nayefroqaya <n.roqaya@gmail.com>
2026-04-13 20:31:39 +0200

    update in the  dataset code

--- src/main.py
+++ src/main.py
@@ -97,7 +97,7 @@ def main():
 
     # ---------------- Project configuration ----------------
 
-    DATASET = 'SP_150MB_ration'
+    DATASET = 'SP_100MB_ratio'
     DATASETS_FOLDER = 'datasets'
     Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
