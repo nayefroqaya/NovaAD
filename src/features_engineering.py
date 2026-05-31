03828086674b834d9dc74647740113e8a60f6dd1
nayefroqaya <n.roqaya@gmail.com>
2026-05-31 22:40:55 +0200

    update

--- src/features_engineering.py
+++ src/features_engineering.py
@@ -98,7 +98,7 @@ class FeaturesEngineering:
 
         if len(xx_anomaly) == 0 or len(yy_normal) == 0:
             print('Error in summing_Train_test_together function: Unlabeled data validation failed')
-            exit()
+            #exit()
 
         summed_df_train = pd.concat([summed_df_normal_labelled_train, summed_df_combine_unlabelled_train])
 
