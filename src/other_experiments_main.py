f2f2a6c580367d254fb43e856c21f018b2416b6b
nayefroqaya <n.roqaya@gmail.com>
2026-06-01 17:50:31 +0200

    update

--- src/other_experiments_main.py
+++ src/other_experiments_main.py
@@ -199,7 +199,7 @@ def main():
     mode = 'M'
 
     SOURCE_DATASETS = ["BGL", "TH_1G"]        # example: ["BGL"], ["BGL", "TH_1G"]
-    TARGET_DATASET = "HDFS"          # example: "HDFS", "SP_150MB_ratio"
+    TARGET_DATASET = "SP_150MB_ratio"          # example: "HDFS", "SP_150MB_ratio"
     TARGET_NORMAL_FRACTION = 0.2     # fraction of normal target-train blocks
     SEED = 123
 
