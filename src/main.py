5fb59938f23b22989b18ad00c1001acbb7cb1f5d
nayefroqaya <n.roqaya@gmail.com>
2026-06-01 06:42:56 +0200

    update

--- src/main.py
+++ src/main.py
@@ -97,7 +97,7 @@ def main():
 
     # ---------------- Project configuration ----------------
 
-    DATASET = 'SP_150MB_ratio'
+    DATASET = 'BGL'
     DATASETS_FOLDER = 'datasets'
     Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
