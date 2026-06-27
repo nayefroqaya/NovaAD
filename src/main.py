9169cb8d0138a4033d1c1a82ebbd3b2f3d5443dc
nayefroqaya <n.roqaya@gmail.com>
2026-06-27 11:20:53 +0200

    update

--- src/main.py
+++ src/main.py
@@ -97,7 +97,7 @@ def main():
 
     # ---------------- Project configuration ----------------
 
-    DATASET = 'BGL'
+    DATASET = 'HDFS'
     DATASETS_FOLDER = 'datasets'
     Round = '1'
     mode = 'M'  # M multi classifier - S single classifier
