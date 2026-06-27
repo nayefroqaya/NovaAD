80b731be9e0bad4dc2e86fb5dfc36567b6f2a535
nayefroqaya <n.roqaya@gmail.com>
2026-06-27 15:26:18 +0200

    update

--- src/features_engineering.py
+++ src/features_engineering.py
@@ -320,7 +320,7 @@ class FeaturesEngineering:
 
         # Train final model with optimal Gamma & Nu
         #oc_svm_final = OneClassSVM(kernel='rbf', gamma=best_params[0], nu=best_params[1])
-        oc_svm_final = OneClassSVM(kernel='rbf', gamma= 0.2, nu=0.0275)
+        oc_svm_final = OneClassSVM(kernel='rbf', gamma= 0.5, nu=0.045)
 
         oc_svm_final.fit(X_train_normal_labelled)
         # Make predictions
