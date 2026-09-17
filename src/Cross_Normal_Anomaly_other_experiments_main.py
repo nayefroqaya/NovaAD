6014e07549c764b56a04a99cac139765adef5e82
nayefroqaya <n.roqaya@gmail.com>
2026-09-17 09:48:32 +0200

    update

--- src/Cross_Normal_Anomaly_other_experiments_main.py
+++ src/Cross_Normal_Anomaly_other_experiments_main.py
@@ -199,7 +199,7 @@ def main():
     mode = 'M'
 
     SOURCE_DATASETS = ["BGL"]        # example: ["BGL"], ["BGL", "TH_1G"]
-    TARGET_DATASET = "TH_1G"          # example: "HDFS", "SP_150MB_ratio"
+    TARGET_DATASET = "HDFS"          # example: "HDFS", "SP_150MB_ratio"
     TARGET_ADAPTATION_FRACTION = 0.2 # fraction of normal AND anomaly target-train blocks
     SEED = 123
 
